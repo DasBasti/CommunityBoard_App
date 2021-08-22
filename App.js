fd7bea7129e0ddd153312e378361846ecbaccb83
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-08-22 22:14:58 +0200

    update with online info and connection error string

--- App.js
+++ App.js
@@ -7,6 +7,26 @@ import { Client, Message } from "react-native-paho-mqtt";
 import PcbPanel from "./PcbPanel";
 import { ScreenOrientation } from "expo";
 
+function guidGenerator() {
+  var S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+}
+
 export default function App() {
   //Set up an in-memory alternative to global localStorage
   const myStorage = {
@@ -20,33 +40,49 @@ export default function App() {
   };
 
   const [pcbString, setPcbString] = useState(
-    "gggggggggwgwggwggwggwgwggwgggwwggwggwgwggggwggwggogwggwggggggggg"
+    "sssssssssssssbsswsssssbsswssssbssswgsbssssgggbsssssgbsbssssssbss"
   );
-
+  const [connectionState, updateConnectionState] = useState(false);
+  const [connectionInfoString, setConnectionInfoString] = useState("");
+  const deviceId = guidGenerator();
   const client = new Client({
     uri: "wss://platinenmacher.tech/mqtt",
-    clientId: "pcbapp-" + Constants.deviceId,
+    clientId: "pcbapp-" + deviceId,
     storage: myStorage,
   });
   client.on("connectionLost", (responseObject) => {
     if (responseObject.errorCode !== 0) {
+      //setConnectionInfoString(responseObject.errorMessage);
       console.log(responseObject.errorMessage);
     }
+    updateConnectionState(false);
   });
   client.on("messageReceived", (message) => {
     setPcbString(message.payloadString);
     console.log(message.payloadString);
   });
 
-  console.log(Constants.deviceId);
   // connect the client
-  client.connect().then(() => {
-    return client.subscribe("pcb/all/stream/enc");
-  });
+  client
+    .connect()
+    .then(() => {
+      return client.subscribe("pcb/all/stream/enc");
+    })
+    .then(() => {
+      updateConnectionState(true);
+    })
+    .catch((responseObject) => {
+      if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+        setConnectionInfoString(responseObject.errorMessage);
+      }
+    });
 
   return (
     <View style={styles.container}>
+      <Text>{connectionState ? "online" : "offline"}</Text>
       <PcbPanel str={pcbString} />
+      <Text>{connectionInfoString}</Text>
       <StatusBar style="auto" />
     </View>
   );
